a21478cb2d3db111e02e023a788691f619ff4f4d
khabaev <khabaev@arrival.com>
2022-04-15 15:04:51 +0300

    implement websockets

--- api/clients/tester_client.py
+++ api/clients/tester_client.py
@@ -1,23 +1,31 @@
+import asyncio
+import configparser
 import json
 
-import websocket
+import websockets
 
 from api.models.base_model import BaseModel
 
+config = configparser.ConfigParser()
+config.read("api/conf/cfg_test_environment.ini")
+ADDRESS = config['WS']['address']
+
 
 class TesterClient:
-    address = "ws://127.0.0.1:4000"
 
     @classmethod
-    def post_to_ws(cls, body):
-        ws = websocket.WebSocket()
-        ws.connect(cls.address)
+    async def get_resp_from_ws(cls, body):
         if not isinstance(body, str):
             if isinstance(body, BaseModel):
                 body = json.dumps(body.dict())
             else:
                 body = json.loads(json.dumps(json.dumps(body, default=lambda x: x.__dict__)))
-        ws.send(body)
-        response = ws.recv()
-        ws.close()
+
+        async with websockets.connect(ADDRESS) as websocket:
+            await websocket.send(body)
+            response = await websocket.recv()
         return response
+
+    @classmethod
+    def post_to_ws(cls, body):
+        return asyncio.get_event_loop().run_until_complete(cls.get_resp_from_ws(body))
